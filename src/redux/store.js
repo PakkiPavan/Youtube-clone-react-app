02a81d3f44f2058def24cbd889ba711b71447437
Pakki Pavan <pavanpakki786@gmail.com>
2021-03-30 20:56:23 +0530

    ready for deployment-2

--- src/redux/store.js
+++ src/redux/store.js
@@ -17,6 +17,7 @@ const rootReducer=combineReducers({
     subscriptionsChannel:subscriptionsChannelReducer,
     channelVideos:channelVideosReducer
 });
+
 const store=createStore(
     rootReducer,
     {},
